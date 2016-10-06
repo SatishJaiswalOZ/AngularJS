23d15f6cc4542c628c41edc471b89535e1ac5c33
satjai1 <itprofessional.satish@gmail.com>
2016-10-07 00:25:37 +0530

    Revert "reference correction"
    This reverts commit 84bdee80984f0ada1a26017521997c82d5ab2b23.

--- PlayWithAngularJS/ngQuickerPrototypeApp/Scripts/controllers.js
+++ PlayWithAngularJS/ngQuickerPrototypeApp/Scripts/controllers.js
@@ -2,7 +2,7 @@
 angular.module('app.controllers', [])
 
     // Path: /
-    .controller('HomeCtrl', ['$rootScope', '$scope', '$location', '$window', 'UserService', 'fileUpload', function ($rootScope, $scope, $location, $window, UserService, fileUpload) {
+    .controller('HomeCtrl', ['$rootScope', '$scope', '$location', '$window', 'UserService','fileUpload', function ($rootScope, $scope, $location, $window, UserService) {
         $scope.$root.title = 'Loan Analytics App';
         var vm = this;
 
